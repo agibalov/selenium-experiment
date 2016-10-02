4f07763310145a114ff78eaed21aee663e32b6b9
loki2302 <loki2302@loki2302.me>
2016-10-01 22:09:34 -0400

    Added Angular1 recipe

--- src/test/java/me/loki2302/AppTest.java
+++ src/test/java/me/loki2302/AppTest.java
@@ -8,8 +8,15 @@ import org.junit.runner.Description;
 import org.junit.runner.RunWith;
 import org.junit.runners.model.Statement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +26,10 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.net.MalformedURLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -55,6 +66,19 @@ public class AppTest {
     }
 
     @Test
+    public void buttonShouldRevealTheMessage_CRUTCH() {
+        final String TEST_MESSAGE = "hello test";
+
+        when(messageProvider.getMessage()).thenReturn(TEST_MESSAGE);
+
+        webDriver.get("http://localhost:8080/");
+        webDriver.findElement(By.tagName("button")).click();
+
+        new WebDriverWait(webDriver, 3).until(elementTextIsNotEmpty(By.tagName("h1")));
+        assertEquals(TEST_MESSAGE, webDriver.findElement(By.tagName("h1")).getText());
+    }
+
+    @Test
     public void buttonShouldRevealTheMessage() {
         final String TEST_MESSAGE = "hello test";
 
@@ -62,8 +86,26 @@ public class AppTest {
 
         webDriver.get("http://localhost:8080/");
         webDriver.findElement(By.tagName("button")).click();
-        new WebDriverWait(webDriver, 3).until(elementTextIsNotEmpty(By.tagName("h1")));
+
+        String syncScript = String.join("\n",
+                "console.log('before');",
+                "var done = arguments[0];",
+                "angular.getTestability(document.body).whenStable(function() { console.log('stable'); done(); });",
+                "console.log('after');");
+
+        ((JavascriptExecutor)webDriver).executeAsyncScript(syncScript);
+
         assertEquals(TEST_MESSAGE, webDriver.findElement(By.tagName("h1")).getText());
+
+        LogEntries logEntries = webDriver.manage().logs().get(LogType.BROWSER);
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        for(LogEntry logEntry : logEntries) {
+            System.out.printf("%s|%s|%s\n",
+                    format.format(new Date(logEntry.getTimestamp())),
+                    logEntry.getLevel(),
+                    logEntry.getMessage());
+        }
     }
 
     private static ExpectedCondition<Boolean> elementTextIsNotEmpty(By by) {
@@ -83,7 +125,16 @@ public class AppTest {
                 @Override
                 public void evaluate() throws Throwable {
                     System.setProperty("webdriver.chrome.driver", "node_modules/chromedriver/bin/chromedriver");
-                    webDriver = new ChromeDriver();
+
+                    LoggingPreferences loggingPreferences = new LoggingPreferences();
+                    loggingPreferences.enable(LogType.BROWSER, Level.ALL);
+
+                    DesiredCapabilities desiredCapabilities = DesiredCapabilities.chrome();
+                    desiredCapabilities.setCapability(CapabilityType.LOGGING_PREFS, loggingPreferences);
+
+                    webDriver = new ChromeDriver(desiredCapabilities);
+                    webDriver.manage().timeouts().setScriptTimeout(5, TimeUnit.SECONDS);
+
                     try {
                         base.evaluate();
                     } finally {
