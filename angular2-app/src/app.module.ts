dc00e1271e5224c67689cc301248adf463302c79
loki2302 <loki2302@loki2302.me>
2016-10-15 21:34:40 -0400

    Angular2 app now makes a request to BE

--- angular2-app/src/app.module.ts
+++ angular2-app/src/app.module.ts
@@ -1,28 +1,50 @@
-import { NgModule } from '@angular/core';
+import {NgModule, Injectable} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Component } from "@angular/core";
+import { HttpModule, Http, Response } from "@angular/http";
+
+interface MessageDto {
+    message: string;
+}
+
+@Injectable()
+class ApiClient {
+    constructor(private http: Http) {
+    }
+
+    async getMessage(): Promise<string> {
+        const response: Response = await this.http.get('http://localhost:8080/api/message').toPromise();
+        const body: MessageDto = response.json();
+        return body.message;
+    }
+}
 
 @Component({
     selector: 'app',
     template: `<div>
-    <h1>hello world {{counter}}</h1>
-    <button type="button" (click)="increment()" class="increase">Increment</button>
+    <h1>message is {{message}}</h1>
+    <button type="button" (click)="loadMessage()" class="load-message">Load Message</button>    
   </div>`
 })
 class AppComponent {
-    public counter: number = 0;
+    public message: string = null;
 
-    async increment(): Promise<void> {
-        ++this.counter;
+    constructor(private apiClient: ApiClient) {
+    }
+
+    async loadMessage(): Promise<void> {
+        this.message = '';
+        const message: string = await this.apiClient.getMessage();
+        this.message = message;
     }
 }
 
 @NgModule({
-    imports: [ BrowserModule ],
+    imports: [ BrowserModule, HttpModule ],
     declarations: [
         AppComponent
     ],
-    providers: [],
+    providers: [ ApiClient ],
     bootstrap: [ AppComponent ]
 })
 export class AppModule {
